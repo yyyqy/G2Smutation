edb2d97fd613627e673744556b9e715d2c03733e
Tangcodes <36091676+Tangcodes@users.noreply.github.com>
2019-01-20 13:42:58 -0600

    Update pdbRepository.java

--- G2Smutation-web/src/main/java/org/cbioportal/G2Smutation/web/database/pdbRepository.java
+++ G2Smutation-web/src/main/java/org/cbioportal/G2Smutation/web/database/pdbRepository.java
@@ -4,8 +4,6 @@ import org.cbioportal.G2Smutation.web.database.pdb_seq_alignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-//This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-//CRUD refers Create, Read, Update, Delete
 public interface pdbRepository extends JpaRepository<pdb_seq_alignment, Integer> {
 	List<pdb_seq_alignment> findByalignmentId(Integer id);
 }
