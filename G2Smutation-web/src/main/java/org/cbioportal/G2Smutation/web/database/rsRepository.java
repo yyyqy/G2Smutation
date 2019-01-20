3124490d8c5097095b519c149ed069a7e315dc3e
Tangcodes <36091676+Tangcodes@users.noreply.github.com>
2019-01-19 22:06:57 -0600

    Update rsRepository.java

--- G2Smutation-web/src/main/java/org/cbioportal/G2Smutation/web/database/rsRepository.java
+++ G2Smutation-web/src/main/java/org/cbioportal/G2Smutation/web/database/rsRepository.java
@@ -1,12 +1,13 @@
 package org.cbioportal.G2Smutation.web.database;
 
 import java.util.List;
-
 import org.cbioportal.G2Smutation.web.database.rs_mutation_entry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface rsRepository extends JpaRepository<rs_mutation_entry, Integer> {
-	List<rs_mutation_entry> findByrsSnpIdBetween(Integer id1,Integer id2);
-	List<rs_mutation_entry> findByrsIdBetween(Integer id1,Integer id2);
+	List<rs_mutation_entry> findByrsSnpId(Integer id);
+	List<rs_mutation_entry> findByseqId(Integer id);
+	List<rs_mutation_entry> findByseqResidue(String id);
+	List<rs_mutation_entry> findBypdbNoStartingWith(String id);
+	List<rs_mutation_entry> findBypdbResidue(String id);
 }
