a12e1e8f3cf96906a0301e026dff2916e1065c53
Tangcodes <36091676+Tangcodes@users.noreply.github.com>
2019-01-25 10:35:51 -0600

    Add files via upload

---
+++ G2Smutation-web/src/main/java/org/cbioportal/G2Smutation/web/domain/MutationRepository.java
@@ -0,0 +1,25 @@
+package org.cbioportal.G2Smutation.web.domain;
+
+import org.cbioportal.G2Smutation.web.database.mutation_usage_table;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+
+public interface MutationRepository extends JpaRepository<mutation_usage_table, Integer> {
+	
+	List<mutation_usage_table> findTop20BymutationIdGreaterThan(Integer id);
+	List<mutation_usage_table> findBypdbNoStartingWith(String id);
+	List<mutation_usage_table> findByseqId(Integer id);
+	
+	/* 3*3 */
+	List<mutation_usage_table> findBySeqResidueOrderByPdbIndex(String id);
+	List<mutation_usage_table> findBypdbIndex(Integer id);
+	List<mutation_usage_table> findBypdbResidueOrderByPdbIndex(String id);
+	
+	List<mutation_usage_table> findBypdbIndexAndPdbResidue(Integer id1,String id2);
+	List<mutation_usage_table> findByseqResidueAndPdbResidue(String id1,String id2);
+	List<mutation_usage_table> findBypdbIndexAndSeqResidue(Integer id1,String id2);
+    
+	List<mutation_usage_table> findBySeqResidueAndPdbResidueAndPdbIndex(String id1,String id2,Integer id3);
+}
+
---
+++ G2Smutation-web/src/main/java/org/cbioportal/G2Smutation/web/domain/PdbRepository.java
@@ -0,0 +1,9 @@
+package org.cbioportal.G2Smutation.web.domain;
+
+import org.cbioportal.G2Smutation.web.database.pdb_seq_alignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PdbRepository extends JpaRepository<pdb_seq_alignment, Integer> {
+	List<pdb_seq_alignment> findByalignmentId(Integer id);
+}
