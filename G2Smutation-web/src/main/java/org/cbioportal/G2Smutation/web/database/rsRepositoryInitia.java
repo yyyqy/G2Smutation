b4328192b8c5237ce0dccc6ae7d2427e2daa2db0
Tangcodes <36091676+Tangcodes@users.noreply.github.com>
2019-01-25 10:34:24 -0600

    Delete rsRepositoryInitia.java

--- G2Smutation-web/src/main/java/org/cbioportal/G2Smutation/web/database/rsRepositoryInitia.java
+++
@@ -1,9 +0,0 @@
-package org.cbioportal.G2Smutation.web.database;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface rsRepositoryInitia extends JpaRepository<rs_mutation_entry_Initia, Integer> {
-	List<rs_mutation_entry_Initia> findTop20ByrsIdAfter(Integer id);
-}
