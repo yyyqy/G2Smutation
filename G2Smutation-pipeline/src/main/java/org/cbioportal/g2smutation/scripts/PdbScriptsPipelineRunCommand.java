7c1d24187719762de8f7d7b16269c9a66344766f
juexinwang <juexinwang@hotmail.com>
2019-04-11 13:22:49 -0500

    Debug 7

--- G2Smutation-pipeline/src/main/java/org/cbioportal/g2smutation/scripts/PdbScriptsPipelineRunCommand.java
+++ G2Smutation-pipeline/src/main/java/org/cbioportal/g2smutation/scripts/PdbScriptsPipelineRunCommand.java
@@ -114,11 +114,11 @@ public class PdbScriptsPipelineRunCommand {
         
         log.info("********************[Init STEP 2]********************");
         log.info("Find mutation using G2S service");
-        //generateMutation(parseprocess);
+        generateMutation(parseprocess);
 
         log.info("********************[Init STEP 3]********************");
         log.info("Annotate mutation");
-        generateAnnotation(parseprocess, false);
+        //generateAnnotation(parseprocess, false);
         
         log.info("********************[Init STEP 4]********************");
         log.info("[FileSystem] Clean Up");
