06933d6aaf84c5e08c4c82190e20e81efc5c600f
juexinwang <juexinwang@hotmail.com>
2019-04-19 18:58:48 -0500

    continue change database

--- G2Smutation-pipeline/src/main/resources/SQL/DATABASE_schema.sql
+++ G2Smutation-pipeline/src/main/resources/SQL/DATABASE_schema.sql
@@ -181,10 +181,10 @@ CREATE TABLE `gpos_allmapping_entry` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `CHR_POS` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL, -- CHR_POSSTART
   `DBSNP_ID` VARCHAR(50) default '',
-  `CLINVAR_ID` VARCHAR(50) default '',
-  `COSMIC_ID` VARCHAR(50) default '',
-  `GENIE_ID` VARCHAR(50) default '',
-  `TCGA_ID` VARCHAR(50) default '',
+  `CLINVAR_ID` VARCHAR(255) default '',
+  `COSMIC_ID` VARCHAR(255) default '',
+  `GENIE_ID` VARCHAR(255) default '',
+  `TCGA_ID` VARCHAR(255) default '',
   PRIMARY KEY(`ID`),
   KEY(`CHR_POS`,`DBSNP_ID`,`CLINVAR_ID`,`COSMIC_ID`,`GENIE_ID`,`TCGA_ID`)
 );
